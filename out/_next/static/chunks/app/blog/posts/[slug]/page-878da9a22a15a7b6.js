8f1a86ae01c31648aae5875ae520b55376da0fde
Simon Bonnier <simonbonnier93@gmail.com>
2023-07-28 14:05:03 +0200

    Remove out folder

--- out/_next/static/chunks/app/blog/posts/[slug]/page-878da9a22a15a7b6.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[831],{4926:function(n,e,u){Promise.resolve().then(u.t.bind(u,5338,23))},5338:function(){}},function(n){n.O(0,[667,488,744],function(){return n(n.s=4926)}),_N_E=n.O()}]);
