311d3170ba26e9ea679345b742fb99e4f5e5a624
Simon Bonnier <simonbonnier93@gmail.com>
2023-07-28 14:10:47 +0200

    Deploy

---
+++ _next/static/chunks/app/layout-bb5e2d2de5d016f2.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{9593:function(n,e,t){Promise.resolve().then(t.t.bind(t,6356,23)),Promise.resolve().then(t.t.bind(t,8920,23))},6356:function(){}},function(n){n.O(0,[920,667,488,744],function(){return n(n.s=9593)}),_N_E=n.O()}]);
--- src/logic/splitClassesFromProps.ts
+++
@@ -1,8 +0,0 @@
-export function splitClassesFromProps<T>(props: React.HTMLAttributes<T>, defaultClasses?: string) {
-    let { className, ...rest } = props;
-    let classes = className?.split(" ") || [];
-    if(defaultClasses) {
-        classes.push(defaultClasses)
-    }
-    return { classes, ...rest }
-}
