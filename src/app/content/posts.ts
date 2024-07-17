9eecc8618aab3fbcb88e46c3ab5050fba321631d
Simon Bonnier <simonbonnier93@gmail.com>
2024-07-17 16:26:35 +0200

    Add two articles

--- src/app/content/posts.ts
+++ src/app/content/posts.ts
@@ -1,5 +1,7 @@
 import { BlogPost } from '@/types/commonTypes';
 import Post1 from '../content/posts/post-1.mdx';
+import EF_Post1 from '../content/posts/part1-setting-up-the-database.mdx';
+import EF_Post2 from '../content/posts/part2-basic-querying.mdx';
 
 export const posts: BlogPost[] = [
     {
@@ -10,5 +12,23 @@ export const posts: BlogPost[] = [
         coverImageUrl: "/blog/post1.jpg",
         date: "2022-11-14T05:35:07.322Z",
         content: Post1
+    },
+    {
+        slug: "EFTutorialPart1",
+        title: "Setting up a development database",
+        category: "Development",
+        summary: "Setting up a postgres db in 5min using docker",
+        coverImageUrl: "/blog/post1.jpg",
+        date: "2024-02-05T00:00:00.000Z",
+        content: EF_Post1
+    },
+    {
+        slug: "EFTutorialPart2",
+        title: "Demysityfy Entity framework queries",
+        category: "Development",
+        summary: "The only articel you need to understand Entity framework",
+        coverImageUrl: "/blog/post1.jpg",
+        date: "2024-02-05T00:00:00.000Z",
+        content: EF_Post2
     }
 ]
