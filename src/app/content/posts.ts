3085ccdbf09a8330eb29419e97f5cfc2010c0fb0
SimonBonnier <simonbonnier93@gmail.com>
2024-07-21 11:42:03 +0200

    Update blog posts and minor css fixes

--- src/app/content/posts.ts
+++ src/app/content/posts.ts
@@ -4,8 +4,8 @@ import EF_Post2 from '../content/posts/part2-basic-querying.mdx';
 
 export const posts: BlogPost[] = [
     {
-        slug: "EFTutorialPart1",
-        title: "Setting up a development database",
+        slug: "setting-up-a-development-database-using-docker",
+        title: "Fast Track to Coding: Set Up Your Development Database in No Time",
         category: "Development",
         summary: "Setting up a postgres db in 5min using docker",
         coverImageUrl: "/blog/post1.jpg",
@@ -13,10 +13,10 @@ export const posts: BlogPost[] = [
         content: EF_Post1
     },
     {
-        slug: "EFTutorialPart2",
-        title: "Demysityfy Entity framework queries",
+        slug: "demystify-ef-what-is-generated-and-when",
+        title: "Demysityfy EF: What is really generated and when is it fetched?",
         category: "Development",
-        summary: "The only articel you need to understand Entity framework",
+        summary: "The only article you need to understand Entity framework",
         coverImageUrl: "/blog/post1.jpg",
         date: "2024-02-05T00:00:00.000Z",
         content: EF_Post2
